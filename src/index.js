33185a5c626546eac6a61fa8dd9db2c25a4dfd80
alisova-projects <63472862+alisova-projects@users.noreply.github.com>
2020-11-18 19:40:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './js/spinner';
 
 import { startSpinner, stopSpinner } from './js/spinner';
 
+// чтобы увидеть спиннер - установи пакет spin.js
+
 //если бесит спиннер - закомментируй)
  startSpinner();
 // stopSpinner();
