439030b773d5bb5bba7abbe282e13b8c5121c680
alisova-projects <63472862+alisova-projects@users.noreply.github.com>
2020-11-21 12:46:14 +0200

    добавила спинер

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
 import "./sass/main.scss";
-import './js/spinner';
-
-import { startSpinner, stopSpinner } from './js/spinner';
-
-// чтобы увидеть спиннер - установи пакет spin.js
-
-//если бесит спиннер - закомментируй)
- startSpinner();
-// stopSpinner();
--- src/js/draw-page.js
+++ src/js/draw-page.js
@@ -1,6 +1,6 @@
 // const API_KEY = 'bb47a995514a49758140b073ef1103f5';
 // const BASE_URL = 'https://developers.themoviedb.org/3';
-
+import loaderToggle from './loader';
 import moviePosterTpl from '../templates/movie-card.hbs';
 import apiService from './api-service';
 
@@ -11,8 +11,11 @@ const newApiService = new apiService();
 
 render();
 
+//добавила спинер
+
 function render() {
-  newApiService.fetchTrendingMovies().then(renderMoviesCard);
+ loaderToggle();
+  newApiService.fetchTrendingMovies().then(renderMoviesCard).then(loaderToggle());
 }
 
 function renderMoviesCard(articles) {
