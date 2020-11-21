0d5414e4312f710bdcb2884f51a1789c670de545
Waxiw92 <63607294+Waxiw92@users.noreply.github.com>
2020-11-21 13:15:04 +0200

    Внес корректировки в импорт

--- src/js/draw-page.js
+++ src/js/draw-page.js
@@ -2,7 +2,7 @@
 // const API_KEY = 'bb47a995514a49758140b073ef1103f5';
 // const BASE_URL = 'https://developers.themoviedb.org/3';
 import loaderToggle from './loader';
-import moviePosterTpl from '../templates/movie-card.hbs';
+import moviePosterTpl from '../templates/movies-list.hbs';
 import apiService from './api-service';
 
 
