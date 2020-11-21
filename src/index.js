629eb7d8b07ae77e34cdb650e147eb6bc7a90ada
alisova-projects <63472862+alisova-projects@users.noreply.github.com>
2020-11-21 12:59:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,5 @@
 import "./sass/main.scss";
 import "./js/modal-footer"
 import "./js/search-movies";
-import './js/spinner';
-import { startSpinner, stopSpinner } from './js/spinner';
 // import './js/modal';
 import "./js/draw-page";
-
-// чтобы увидеть спиннер - установи пакет spin.js
-
-//если бесит спиннер - закомментируй)
- startSpinner();
-// stopSpinner();
-
